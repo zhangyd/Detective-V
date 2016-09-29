6ee2b960c094816707312588b2cab5bfb18cb6d4
zhangyd <zhangyd@umich.edu>
2016-09-29 00:19:04 -0400

    WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
 	# def store_location
 	#   # store last url - this is needed for post-login redirect to whatever the user last visited.
-	#   return unless request.get? 
+	#   return unless request.get?
 	#   if (request.path != "/users/sign_in" &&
 	#       request.path != "/users/sign_up" &&
 	#       request.path != "/users/password/new" &&
@@ -16,16 +16,18 @@ class ApplicationController < ActionController::Base
 	#       request.path != "/users/confirmation" &&
 	#       request.path != "/users/sign_out" &&
 	#       !request.xhr?) # don't store ajax calls
-	#     session[:previous_url] = request.fullpath 
+	#     session[:previous_url] = request.fullpath
 	#   end
 	# end
 
 	# def after_sign_in_path_for(resource)
 	#   session[:previous_url] || root_path
 	# end
-	
+
 	def get_repos
-		@repos = current_user.repos
+		if current_user
+			@repos = current_user.repos
+		end
 	end
 
   # def after_sign_in_path_for(resource)
