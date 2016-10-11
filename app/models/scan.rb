f668ec237da7821532ad58ba7be9318a997e5f61
zhangyd <zhangyd@umich.edu>
2016-10-10 22:34:30 -0400

    user, scan, issues model fixes

--- app/models/scan.rb
+++ app/models/scan.rb
@@ -1,5 +1,6 @@
 class Scan < ActiveRecord::Base
   belongs_to :repo
+  belongs_to :user
   has_many :issues, dependent: :destroy
   validates :repo_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
   has_many :repos, dependent: :destroy
+  has_many :scans
+  has_many :issues
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:github]
-  
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
