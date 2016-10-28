47ddd502fe65678673640ebf2b5c729815c6ba2e
L Q <qinlihui@umich.edu>
2016-10-28 09:58:54 -0400

    issue #31 add actural  severity color scheme

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,2 +1,15 @@
 module IssuesHelper
+	def class_for_severity(severity)
+		color_map = {
+			0 => 'info', 
+			1 => 'info',
+			2 => 'warning',
+			3 => 'warning',
+			4 => 'warning',
+			5 => 'danger',
+			6 => 'danger',
+			7 => 'danger'
+		}
+		return color_map[severity]
+	end
 end
